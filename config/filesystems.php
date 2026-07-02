de094aa295a89217e3734c95440e9c731dc71af5
chhatrapatikumar <kumarchhatrapati07@gmail.com>
2026-07-02 20:25:03 +0530

    did composer update

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,19 +48,24 @@ return [
         ],
 
         's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', true),
-            'throw' => false,
 
-            // Crucial for Backblaze B2 compatibility to prevent 400 Errors:
-            'request_checksum_calculation' => 'when_required',
-            'response_checksum_validation' => 'when_required',
+            'driver' => 's3',
+
+            'key' => env('AWS_ACCESS_KEY_ID'),
+
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+
+            'region' => env('AWS_DEFAULT_REGION'),
+
+            'bucket' => env('AWS_BUCKET'),
+
+            'endpoint' => env('AWS_ENDPOINT'),
+
+            'url' => env('AWS_URL'),
+
+            'use_path_style_endpoint' => true,
+
+            'throw' => true,
         ],
 
     ],
